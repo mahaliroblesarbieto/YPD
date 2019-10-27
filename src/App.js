b8beb3de77ea0708e8bcfa60da1c0180214868c0
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-10-27 17:18:56 -0500

    implementando primeflex

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginContainer from './container/LoginContainer';
+
+function App() {
+  return (
+    <>
+    <LoginContainer/>
+    </>
+  );
+}
+
+export default App;
